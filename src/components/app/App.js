fa1d18e327ecf4a773201c334afdf17f4223b563
igralex1 <igralex1@gmail.com>
2019-05-28 07:10:55 +0300

    first test commit

---
+++ src/components/app/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+<article>
+  <h1>ToDo</h1>
+    <section>
+      <>
+      <h2>Lorem Ipsum</h2>
+      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+    </section>   
+</article>
+
+     
+    </div>
+  );
+}
+
+export default App;
+
+
